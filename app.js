4501ca302eb2ec0b3b236443f02e32c30c7ca836
Prakash Rai <60139321+Prakash171999@users.noreply.github.com>
2025-04-15 21:46:19 +0545

    Feat: Mongodb setup and db connection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -15,8 +16,10 @@ app.get("/", (req, res) => {
   res.send("Hello from the server! After long time.");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
+
+  await connectToDatabase();
 });
 
 export default app;
