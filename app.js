7bd6b2ffce3718c593681c0c8185818a1d7c233a
Prakash Rai <60139321+Prakash171999@users.noreply.github.com>
2025-04-15 18:15:50 +0545

    Feat: Setup routes for auth, subscription and user

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 const app = express();
 
+// middleware -> which route you wanna use.
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
 app.get("/", (req, res) => {
-    res.send("Hello from the server! After long time.");
+  res.send("Hello from the server! After long time.");
 });
 
-app.listen(3000, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
 export default app;
