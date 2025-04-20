5ceb463fcaf959962c4dacd7cfe433d530890ecc
Prakash Rai <60139321+Prakash171999@users.noreply.github.com>
2025-04-20 21:56:18 +0545

    Feat: Implementing errorMiddleware and default express middlewares

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json()); // Parse JSON request bodies and attach them to req.body
+app.use(express.urlencoded({ extended: false })); // Parse URL-encoded request bodies sent via HTML forms in a simple format
+app.use(cookieParser()); // Reads cookies from incoming requests, so the app can store user data
 
 // middleware -> which route you wanna use.
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Hello from the server! After long time.");
