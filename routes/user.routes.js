1f413bdab0940bf8ca00d120fe7fc3174e2ebefb
Prakash Rai <60139321+Prakash171999@users.noreply.github.com>
2025-04-22 01:24:59 +0545

    Feat: GetUsers and getOneUser with authorization

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,25 +1,23 @@
 import { Router } from "express";
+import { getUsers, getUser } from "../controllers/user.controlller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => {
-    res.send({ title: "GET all users" });
-});
+userRouter.get("/", authorize ,getUsers);
 
-userRouter.get("/:id", (req, res) => {
-    res.send({ title: "GET a user details" });
-});
+userRouter.get("/:id", authorize, getUser);
 
 userRouter.post("/", (req, res) => {
-    res.send({ title: "CREATE a new user" });
+  res.send({ title: "CREATE a new user" });
 });
 
 userRouter.put("/:id", (req, res) => {
-    res.send({ title: "UPDATE a user" });
+  res.send({ title: "UPDATE a user" });
 });
 
 userRouter.delete("/:id", (req, res) => {
-    res.send({ title: "DELETE a user" });
+  res.send({ title: "DELETE a user" });
 });
 
 export default userRouter;
